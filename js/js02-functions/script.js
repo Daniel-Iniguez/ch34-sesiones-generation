a9ca906cff5e21c578bde54e41781631aec75fbc
Daniel Iñiguez <danielig9@outlook.com>
2023-11-13 18:10:17 -0600

    Funciones flecha

--- js/js02-functions/script.js
+++ js/js02-functions/script.js
@@ -45,7 +45,46 @@ const sum = function sumaDeEnteros(a,b){
 console.log(sum (6,20)); //26
 
 
-const potencia = function(base,potencia){
-    return base**potencia;
+const potencia = function(base,exponente){
+    return base**exponente;
 };
 console.log(potencia(3,2)); //9
+
+const power = (base, exponent) => base ** exponent;
+
+//----------------Funcion autoinvocadas-----------
+// (selft-incoking functions)
+// Se autoinvocan, no necesitan un llamado
+// Se pueden definir con funciones anonimas
+
+(function setUp(){
+    console.log("Me autoinvoco");
+    console.log("Puedo servirte como inicializador de tu programa");
+})
+
+//----------Funcion Flecha-----------------------
+// (arrow function)
+/*
+Son similares a las funciones expresadas, pero:
+- No requieren la palabra function.
+- Si tiene una sola instrucción, no requeire las llaves {}
+-Si la instruccion es el mismo retorno, no requiere la palabra return.
+
+*/
+
+//Area de rectangulo con funcion expresada
+const areaRectangulo = function(base,altura){
+    return base*altura;
+};
+
+//Area del rectangulo con arrow function
+const areaRectangulo2 = (base,altura) => base*altura;
+
+console.log('El area es: ',areaRectangulo(10,6)); // 60
+console.log('El area es: ',areaRectangulo2(5,4)); // 20
+
+const saludo = persona => `Hola ${persona}`;
+const ellaBailaSola = () => `Compa, que le parece esa morra`
+
+console.log( saludo('Peso Pluma'));
+console.log( ellaBailaSola());
