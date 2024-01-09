1d9629ed060ec0e924682b6db31a1ca4ff74b840
Daniel Iñiguez <danielig9@outlook.com>
2024-01-09 15:19:13 -0600

    SpringBoot

--- java/eclipse/sp2_injection_mv/src/main/java/org/generation/app/model/User.java
+++ java/eclipse/sp2_injection_mv/src/main/java/org/generation/app/model/User.java
@@ -1,14 +1,24 @@
 package org.generation.app.model;
 
-import lombok.AllArgsConstructor;
+import java.util.HashMap;
+import java.util.Map;
 
-@AllArgsConstructor
+import org.springframework.stereotype.Component;
+
+import lombok.*;
+
+@AllArgsConstructor // contructor con parámetros
+@NoArgsConstructor // contructor vacío
+@Setter
+@Getter
+@ToString
+@Component
 public class User {
 	
+	
 	private long id;
-	private String firstName;
-	private String lasttName;
-	private int cohorte;
+	private String firstname;
+	private String lastname;
 	private static int counter;
 	
 	{
@@ -18,5 +28,60 @@ public class User {
 	
 	
 	
+	public User(long id, String firstname, String lastname) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public static int getCounter() {
+		return counter;
+	}
+
+	public static void setCounter(int counter) {
+		User.counter = counter;
+	}
+
+	public String greeting() {
+		return "Hola soy Usuario general";
+	}
+	
+	public static Map<Integer, User> usersMock(){
+		Map<Integer, User> users = new HashMap<>();
+		users.put( 100, new User(100, "Mario", "M") );
+		users.put( 101, new User(101, "Miguel", "O") );
+		users.put( 102, new User(102, "Liliana", "O") );
+		users.put( 103, new User(103, "Alex", "E") );
+		users.put( 104, new User(104, "Gabriela", "F") );
+		users.put( 105, new User(105, "Geo", "F") );
+		users.put( 106, new User(106, "Rafa", "C") );
+		
+		return users;
+	}
 	
 }
